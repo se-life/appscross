4431aa6d6bccac531811393510d44a0d1c22d9dc
laoe <evan.awn@outlook.com>
2023-07-09 00:38:17 +0800

    Update tuic5.sh

--- tuic5.sh
+++ tuic5.sh
@@ -383,7 +383,7 @@ EOF
 	green "创建完成！" && sleep 0.3
 	ee "【${GREEN}6${PLAIN}】Tuic安装部署完成！"
 	ee "[${GREEN}6-1${PLAIN}]tuic内核和配置文件均安装于${GREEN}/usr/local/bin/tuic/${PLAIN}目录"
-	slepp 1
+	sleep 1
 	ee "[${GREEN}6-2${PLAIN}]Tuic安装部署完成，服务端配置文件如下：" 
 	cat /usr/local/bin/tuic/config.json
 	read -n 1 -s -r -p "按任意键退出安装程序，脚本将自动启动Tuic服务..."
